e979b58eda4e361f55c8cc9ba404f9d26ed82407
MD:Rubrl RM <inforubel251@gmail.com>
2023-07-23 18:32:17 +0600

    ahow the admission pages data

---
+++ src/Components/AdmissionCart.jsx
@@ -0,0 +1,40 @@
+
+
+const AdmissionCart = ({ my_college }) => {
+    console.log(my_college);
+    const { Address, BirthDay, CollageImg, College_name, Events, History, Phone, Sports, StudentEmail, StudentName, Student_photo, Subject } = my_college || {}
+    return (
+        <div className="card card-side bg-blue-50 shadow-xl ">
+            <figure><img className='w-72 h-full' src={CollageImg} alt="Movie" /></figure>
+            <div className="card-body">
+                <h2 className="font-semibold">{College_name}</h2>
+                <p className="text-sm">{History}</p>
+                <span className="flex">
+                    <small> <span className="font-bold">Events</span>: <br />{Events}</small>
+                    <small> <span className="font-bold">Sports</span>:<br /> {Sports}</small>
+                </span>
+                <hr />
+                <div className="flex gap-12 items-center">
+                    <h3 className="font-semibold"> My Info</h3>
+                    <div className="avatar">
+                        <div className="w-10 rounded-full">
+                            <img src={Student_photo} />
+                        </div>
+                    </div>
+                </div>
+                <div className="flex gap-2 font-bold">
+                    <small>{StudentName}</small>
+                    <small>{StudentEmail}</small>
+                    <small>{Phone}</small>
+                </div>
+                <div className="flex gap-2 font-bold">
+                    <small>{Address}</small>
+                    <small>{BirthDay}</small>
+                    <small>{Subject}</small>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdmissionCart;
--- src/Pages/My Collages/My_Collage.jsx
+++ src/Pages/My Collages/My_Collage.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import AdmissionCart from "../../Components/AdmissionCart";
 
 
 const My_Collage = () => {
+    const [myColleges, setMyColleges] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/my_admissions')
+            .then(res => res.json())
+            .then(result => {
+                setMyColleges(result);
+            })
+    }, []);
     return (
-        <div>
-            My_Collage
+        <div className="grid md:grid-cols-2  gap-6  py-10">
+            {
+                myColleges.map(my_college => <AdmissionCart
+                    key={my_college._id}
+                    my_college={my_college}
+                ></AdmissionCart>)
+            }
         </div>
     );
 };
